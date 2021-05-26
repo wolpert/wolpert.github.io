73b5fa0bae4278064ad2fdfbdf9baa3e44f7b2c4
Ned Wolpert <ned.wolpert@gmail.com>
2021-05-26 08:05:13 -0700

    org-mode

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210525.1518';
+const cacheName = 'chirpy-20210526.1504';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
