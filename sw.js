9e45a6609bb49c37c3605986b6b389afdf527936
Ned Wolpert <ned.wolpert@gmail.com>
2021-07-24 18:02:01 -0700

    Rusty

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210612.1454';
+const cacheName = 'chirpy-20210725.0101';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
