919771aaf09727a9ced9de483ae7f1354068d031
Ned Wolpert <ned.wolpert@gmail.com>
2021-12-10 07:44:23 -0700

    Added notes about the new framework laptop

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210725.0101';
+const cacheName = 'chirpy-20211210.1443';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
