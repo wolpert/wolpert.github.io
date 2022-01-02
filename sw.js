f261680d5587bcc6af27e4da4eb436f531a3107b
Ned Wolpert <ned.wolpert@gmail.com>
2022-01-02 09:36:58 -0700

    Final framework post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211217.0212';
+const cacheName = 'chirpy-20220102.1636';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
