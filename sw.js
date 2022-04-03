59984f3a0eeaccc8080703ff85acf7894cdb3afa
Ned Wolpert <ned.wolpert@gmail.com>
2022-04-03 08:52:26 -0700

    Violet Keys

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220129.1841';
+const cacheName = 'chirpy-20220403.1551';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
