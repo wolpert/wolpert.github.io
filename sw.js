571f6ff94cd24e6b7211994397e7d8255cf55304
Ned Wolpert <ned.wolpert@gmail.com>
2022-09-20 07:37:26 -0700

    Gamelib

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220807.1941';
+const cacheName = 'chirpy-20220920.1436';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
