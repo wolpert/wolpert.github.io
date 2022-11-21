129a3af0c0c6553ece8b9daf30fe20c44f4ee05c
Ned Wolpert <ned.wolpert@gmail.com>
2022-11-21 07:57:19 -0700

    Twitte

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220920.1436';
+const cacheName = 'chirpy-20221121.1456';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
