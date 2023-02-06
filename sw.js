17eabaeb4bfa308386f97cddfc12b4c02ad3cf8a
Ned Wolpert <ned.wolpert@gmail.com>
2023-02-06 08:16:33 -0700

    Added dstore

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221121.1456';
+const cacheName = 'chirpy-20230206.1516';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
