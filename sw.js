3aadb1f812a1e21322330d9b0f187ba319204b53
Ned Wolpert <ned.wolpert@gmail.com>
2023-07-29 09:06:00 -0700

    July 29, 2023

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230206.1516';
+const cacheName = 'chirpy-20230729.1605';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
