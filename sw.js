48ad43c235f47ecf583d14f22305ed93d78a3a69
Ned Wolpert <ned.wolpert@gmail.com>
2024-03-24 16:56:30 -0700

    Fixed up website

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240308.1539';
+const cacheName = 'chirpy-20240324.2355';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
