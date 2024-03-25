fcf97b118013684b26c92af73a7571188ef5b49b
Ned Wolpert <ned.wolpert@gmail.com>
2024-03-24 17:06:05 -0700

    Fixed up mastodon link

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240324.2355';
+const cacheName = 'chirpy-20240325.0005';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
