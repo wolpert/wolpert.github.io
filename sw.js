c234975069ea4d70088ef93ce5a508ea5ca7a230
Ned Wolpert <ned.wolpert@gmail.com>
2024-04-02 12:17:29 -0700

    Added a second timeline analysis

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240331.1653';
+const cacheName = 'chirpy-20240402.1916';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
