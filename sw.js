dae16c15d3a0a4f5b0f5f03774200851995d7864
Ned Wolpert <ned.wolpert@gmail.com>
2025-04-02 05:52:53 -0700

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240402.1916';
+const cacheName = 'chirpy-20250402.1251';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
